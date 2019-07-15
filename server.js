4ce0baf5058bf9acac29c9bc81af22af04b7480c
viloria <viloria_f@yahoo.com.mx>
2019-07-15 14:04:28 -0500

    70. Usando Helpers

--- server.js
+++ server.js
@@ -5,17 +5,31 @@ app.use(express.static(__dirname + '/public'));
 //Express HBS
 hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
+// //helpers
+require('./hbs/helpers');
+// hbs.registerHelper('getAnio', () => {
+//     return new Date().getFullYear();
+// });
+// hbs.registerHelper('capitalizar', (texto) => {
+//     let palabras = texto.split(' ');
+//     palabras.forEach((palabra, idx) => {
+//         palabras[idx] = palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
+//     });
+//     return palabras.join(' ');
+// })
+
+
 app.get('/', (req, res) => {
     res.render('home', {
-        nombre: 'Victoria',
-        anio: new Date().getFullYear()
+        nombre: 'vICToriAa',
+        // anio: new Date().getFullYear()
     });
 });
 
 app.get('/about', (req, res) => {
     res.render('about', {
         nombre: 'felipe',
-        anio: new Date().getFullYear()
+        // anio: new Date().getFullYear()
     });
 });
 app.listen(3000, () => {
