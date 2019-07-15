edd347dc4ee8b8507a18b43043e72fb297738469
viloria <viloria_f@yahoo.com.mx>
2019-07-15 13:11:10 -0500

    60. Usando parciales con HBS

--- server.js
+++ server.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
 app.use(express.static(__dirname + '/public'));
 //Express HBS
+hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
 app.get('/', (req, res) => {
     res.render('home', {
         nombre: 'Victoria',
         anio: new Date().getFullYear()
     });
-})
+});
 
+app.get('/about', (req, res) => {
+    res.render('about', {
+        nombre: 'felipe',
+        anio: new Date().getFullYear()
+    });
+});
+app.listen(3000, () => {
+    console.log('escuchando peticiones en el puerto 3000');
+});
 // app.get('/', function (req, res) {
 //   res.send('Hello World')
 // })
@@ -25,9 +36,7 @@ app.get('/', (req, res) => {
 //     };
 //     res.send(salida);
 // });
-app.get('/data', (req, res) => {
-    res.send('hola data');
-})
-app.listen(3000, () => {
-    console.log('escuchando peticiones en el puerto 3000');
-});
+
+// app.get('/data', (req, res) => {
+//     res.send('hola data');
+// })
